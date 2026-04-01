687d3b8bb8d5ac4004aa763e8b77cf3d5a025003
Kinshuk699 <kinshuk.sahni@learner.manipal.edu>
2026-04-01 16:07:54 +0530

    Initial Commit

---
+++ sEYEght/sEYEghtApp.swift
@@ -0,0 +1,17 @@
+//
+//  sEYEghtApp.swift
+//  sEYEght
+//
+//  Created by Kinshuk on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct sEYEghtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sEYEghtTests/sEYEghtTests.swift
@@ -0,0 +1,16 @@
+//
+//  sEYEghtTests.swift
+//  sEYEghtTests
+//
+//  Created by Kinshuk on 4/1/26.
+//
+
+import Testing
+
+struct sEYEghtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
